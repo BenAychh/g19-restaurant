22c106badadc0f6300270946301d27bfd534f803
BenAychh <ben.tomas.hernandez@gmail.com>
2016-02-28 14:12:36 -0700

    Deployed link

--- config.js
+++
@@ -1,4 +0,0 @@
-var connectionString = process.env.DATABASE_URL ||
-'postgres://lbgjnucwkuqoaq:5mrUwlKfk-vrpJTGQyyWgd9rSt@ec2-54-225-197-143.compute-1.amazonaws.com:5432/ddt1sg1befeoji';
-
-module.exports = connectionString;
