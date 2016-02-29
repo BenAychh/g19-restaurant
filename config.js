598f9508aa372b98c1ad7ab9ba1381660b51dcc1
BenAychh <ben.tomas.hernandez@gmail.com>
2016-02-29 11:04:14 -0700

    Trying to fix lack of uploaded credentials

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
-var credentials = require('./credentials.js');
-var connectionString = process.env.DATABASE_URL ||
-'postgres://' +
-credentials.username + ':' + credentials.password +
-'@localhost:5432/restaurants';
+var connectionString = '';
+try {
+  var credentials = require('./credentials.js');
+  connectionString = process.env.DATABASE_URL ||
+  'postgres://' +
+  credentials.username + ':' + credentials.password +
+  '@localhost:5432/restaurants';
+} catch (e) {
+  connectionString = process.env.DATABASE_URL;
+}
 module.exports = connectionString;
