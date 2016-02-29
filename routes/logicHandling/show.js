fe9ab0429238af2299bbed916d0d7ade19cabdaa
BenAychh <ben.tomas.hernandez@gmail.com>
2016-02-29 12:03:53 -0700

    Fixing paragraph issue in reviews

--- routes/logicHandling/show.js
+++ routes/logicHandling/show.js
@@ -15,6 +15,16 @@ function startReviewGrabbing(res, pListOfRestaurants) {
 }
 function display(res, reviews) {
   var name = listOfRestaurants[0].name;
+  reviews = reviews.map(function(review) {
+    var tempObj = {};
+    tempObj.text = review.text.split('\n').join('<br>').split('\r').join('<br>');
+    tempObj.created_date = review.created_date;
+    tempObj.modified_date = review.modified_date;
+    tempObj.restaurant_id = review.restaurant_id;
+    tempObj.rating = review.rating;
+    tempObj.id = review.id;
+    return tempObj;
+  });
   var params = {
     'title': 'Fake Yelp! - ' + name,
     'header': name,
